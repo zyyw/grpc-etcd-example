00b91d718b8604e08dbc2e154339831951703ad1
Shengwen Yu <yushengwen@tron.network>
2020-03-10 17:52:43 +0800

    user proto

--- grpcclient/helloworldclient.go
+++ grpcclient/helloworldclient.go
@@ -3,18 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/zyyw/grpc-etcd-example/expample/pb2"
 	"time"
 
-	"strconv"
-
 	grpclb "github.com/zyyw/grpc-etcd-example/balance"
-	"github.com/zyyw/grpc-etcd-example/expample/pb"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
 var (
-	serv = flag.String("service", "hello_service", "service name")
+	serv = flag.String("service", "user_service", "service name")
 	reg  = flag.String("reg", "http://127.0.0.1:2379", "register etcd address")
 )
 
@@ -25,20 +23,36 @@ func main() {
 	b := grpc.RoundRobin(r)
 
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	conn, err := grpc.DialContext(ctx, *reg, grpc.WithInsecure(), grpc.WithBalancer(b))
+	conn, err := grpc.DialContext(ctx, *reg, grpc.WithInsecure(), grpc.WithBalancer(b), grpc.WithBlock())
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("conn...")
 
-	ticker := time.NewTicker(1 * time.Second)
-	for t := range ticker.C {
-		client := pb.NewGreeterClient(conn)
-		resp, err := client.SayHello(context.Background(), &pb.HelloRequest{Name: "world " + strconv.Itoa(t.Second())})
+	//ticker := time.NewTicker(1 * time.Second)
+	//for range ticker.C {
+		client := pb2.NewUserServiceClient(conn)
+		resp, err := client.SetUserAutopay(context.Background(), &pb2.AutopayRequest{
+			UserAddress: "TTCXimHXjen9BdTFW5JvcLKGWNm3SSuECF",
+			Code:        pb2.AutopayRequest_YES,
+		})
 		if err == nil {
-			fmt.Printf("%v: Reply is %s\n", t, resp.Message)
+			fmt.Printf("Reply is %s\n", resp.Message)
 		} else {
+			fmt.Println("AAAA")
 			fmt.Println(err)
 		}
-	}
+	//}
+
+	//ticker := time.NewTicker(1 * time.Second)
+	//for t := range ticker.C {
+	//	client := pb.NewGreeterClient(conn)
+	//	resp, err := client.SayHello(context.Background(), &pb.HelloRequest{Name: "world " + strconv.Itoa(t.Second())})
+	//	if err == nil {
+	//		fmt.Printf("%v: Reply is %s\n", t, resp.Message)
+	//	} else {
+	//		fmt.Println("AAAA")
+	//		fmt.Println(err)
+	//	}
+	//}
 }
